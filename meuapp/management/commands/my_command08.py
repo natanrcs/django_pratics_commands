4c846f59b013dd28d82455d08edf0b75cee88797
Natan Santos <nathanrcs2022@gmail.com>
2026-08-12 15:55:15 -0300

    criando um script sem framework,script puro em python

---
+++ meuapp/management/commands/my_command08.py
@@ -0,0 +1,33 @@
+"""fazendo o mesmo desafio sem framework"""
+import argparse
+import requests
+
+def main():
+    parser = argparse.ArgumentParser(description="fazendo desafio do pablo sem frame-work")
+    parser.add_argument("--cep",type=str,required=True)
+    parser.add_argument("--natan",type=str,required=True)
+
+    args = parser.parse_args()
+    cep= args.cep.replace("-","").strip()
+    url= f"https://viacep.com.br/ws/{cep}/json/"
+    try:
+        resq= requests.get(url)
+        if resq.status_code == 200:
+            data= resq.json()
+            result={
+                "cep": data.get("cep"),
+                "logradouro": data.get("logradouro"),
+                "bairro": data.get("bairro"),
+                "uf": data.get("uf"),
+                "regiao": data.get("regiao"),
+                "ibge": data.get("ibge")
+            }
+            print(f"result in request: {result}")
+        else:
+            print(f"error in request:{resq.status_code}")
+    except ValueError as error:
+        print(f"error in connection:{error}")
+
+if __name__ == "__main__":
+    print("fiz sozinho")
+    main()
