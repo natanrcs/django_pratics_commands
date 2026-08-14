98505da38d250974171f4c723a41217e622ae2a5
Natan Santos <nathanrcs2022@gmail.com>
2026-08-14 16:02:02 -0300

    meu script feito com sucesso,feliz sextou

--- meuapp/management/commands/my_command09.py
+++ meuapp/management/commands/my_command09.py
@@ -1,5 +1,19 @@
 """Script para listar minhas images do ACR e deletar manifest"""
+from azure.containerregistry import ContainerRegistryClient
+from azure.identity import DefaultAzureCredential
+
+endpoint_the_url= "https://devnatanrcs.azurecr.io"
+credential= DefaultAzureCredential()
+try:
+    client= ContainerRegistryClient(endpoint_the_url,credential)
+except ValueError as error:
+    print(f"error in connection {error}")
 
 
+def read_list_repositorys():
+    for all_repository in client.list_repository_names():
+        print(f"all repository in acr: {all_repository}")
 
+if __name__ == "__main__":
+    read_list_repositorys()
 
