bca0468db98aebc8ca6daacea95f95ca5ba93c82
Natan Santos <nathanrcs2022@gmail.com>
2026-08-14 15:16:09 -0300

    script funcionando perfeitamente e testado

--- meuapp/management/commands/my_command08.py
+++ meuapp/management/commands/my_command08.py
@@ -1,4 +1,5 @@
 """fazendo o mesmo desafio sem framework"""
+"""consulta no azure,lista todas as imagens do acr,quais libs se conecta ao azure"""
 import argparse
 import requests
 from pprint import pformat
@@ -6,10 +7,10 @@ from pprint import pformat
 def main():
     parser= argparse.ArgumentParser(description="Desafio do Pablito script sem Framework!")
     parser.add_argument("--cep",type=str,required=True)
-    parser.add_argument("--fiz-sozinho",type=str,required=True)
+    parser.add_argument("--natan",type=str,required=True)
     arguments= parser.parse_args()
     cep= arguments.cep.replace("-","").strip()
-    url= url = f"https://viacep.com.br/ws/{cep}/json/"
+    url=f"https://viacep.com.br/ws/{cep}/json/"
     if len(cep) != 8 or not cep.isdigit():
         messager_error= f"error: cep deve contem 8 digitos"
         print(messager_error)
