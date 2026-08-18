0e405831955be099421bd48f2eccfeaa9c62e7a7
Natan Santos <nathanrcs2022@gmail.com>
2026-08-18 15:47:40 -0300

    arrumando nome de variavel errada

--- meuapp/management/commands/my_command09.py
+++ meuapp/management/commands/my_command09.py
@@ -1,7 +1,7 @@
 """Script para listar minhas images do ACR e deletar manifest"""
 from azure.containerregistry import ContainerRegistryClient
 from azure.identity import DefaultAzureCredential
-from azure.core.exceptions import HttpResponseError
+from azure.core.exceptions import HttpResponseError, Re
 endpoint_url="https://devnatanrcs.azurecr.io" 
 credential= DefaultAzureCredential()
 
@@ -11,13 +11,13 @@ except ValueError as error:
     raise ValueError(f"invalid client configuration: {error}") from error
 
 
-def delete_manifest(repository,digest):
+def delete_manifest(repository,tag):
     try:
-        client.delete_manifest(repository,digest)
+        client.delete_manifest(repository,tag)
         print(f"manifest deleted sucess:{repository}")
     except HttpResponseError as error:
         raise HttpResponseError(message=f"error in delete manifest {error}") from error
 
 
 if __name__ == "__main__":
-    delete_manifest("alpine","")
+    delete_manifest("alpine","3.18")
